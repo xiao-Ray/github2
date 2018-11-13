46c8d6a6a63fb800be6d77757fd8853464ef11af
xiao-Ray <40203412+xiao-Ray@users.noreply.github.com>
2018-11-13 14:27:32 +0800

    Update connectTest1.java

--- gitTest1/src/main/java/connectTest1.java
+++ gitTest1/src/main/java/connectTest1.java
@@ -1,5 +1,5 @@
 public class connectTest1 {
     public static void main(String[] args) {
-        System.out.println("这是第一次ideal连接测试1");
+        System.out.println("这是第一次ideal连接测试1---从github中clone and merge");
     }
 }
