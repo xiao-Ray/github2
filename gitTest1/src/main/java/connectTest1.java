cffd97f897187097d5c7ad59fec87dfa368cced9
ray <1083903857@qq.com>
2018-11-13 15:03:23 +0800

    提交修改master1次

--- gitTest1/src/main/java/connectTest1.java
+++ gitTest1/src/main/java/connectTest1.java
@@ -1,6 +1,6 @@
 public class connectTest1 {
     public static void main(String[] args) {
         System.out.println("这是第一次ideal连接测试1---从github中clone and merge");
-        System.out.println("修改了localhostDev1的分支测试4次");
+        System.out.println("修改了localhostDev1的分支测试3次");
     }
 }
