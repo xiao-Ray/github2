1468a8e36153ff6e8a6f770548efdc5f9a4953cb
ray <1083903857@qq.com>
2018-11-13 15:11:39 +0800

    连接gitee测试1次

--- gitTest1/src/main/java/connectTest1.java
+++ gitTest1/src/main/java/connectTest1.java
@@ -1,6 +1,5 @@
 public class connectTest1 {
     public static void main(String[] args) {
-        System.out.println("这是第一次ideal连接测试1---从github中clone and merge");
-        System.out.println("修改了localhostDev1的分支测试3次");
+        System.out.println("这是第一次ideal连接gitee测试1次");
     }
 }
