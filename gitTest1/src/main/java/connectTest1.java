d060ea5507622bbc16e004f7407efe52f264cc6b
ray <1083903857@qq.com>
2018-11-13 14:36:03 +0800

    提交分支localhostdev1

--- gitTest1/src/main/java/connectTest1.java
+++ gitTest1/src/main/java/connectTest1.java
@@ -1,5 +1,6 @@
 public class connectTest1 {
     public static void main(String[] args) {
         System.out.println("这是第一次ideal连接测试1---从github中clone and merge");
+        System.out.println("创建了localhostDev1的分支1次");
     }
 }
